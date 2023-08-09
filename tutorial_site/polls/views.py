bd42a97ce0d9c0c61ea206c9ed9f0c462c1227f2
Handle Home <handle.huang@outlook.com>
2023-08-09 21:46:45 +0800

    tutorial_03

--- tutorial_site/polls/views.py
+++ tutorial_site/polls/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404, HttpResponse
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Polls")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
 
 def home(request):
     return HttpResponse("This is the Polls home page")
+
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
